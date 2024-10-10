4c391b2f30da96efefb0c2252c58225d26925db3
Shamil Abdullin <abdullin.shamil100@gmail.com>
2024-10-10 17:36:36 +0300

    main page added

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,100 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.sass'
+import Logo from '@/public/logo.svg'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [source, setSource] = useState('operators')
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleSource = (source: string) => {
+    setSource(source)
+  }
+
+  return (
+    <div className={styles.home}>
+      <header className={styles.home__header}>
+        <div className={styles.home__header__logo}>
+          <Image src={Logo} alt="logo" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.home__header__menu}>
+          <button className={styles.home__header__menu__item}>Преимущества</button>
+          <button className={styles.home__header__menu__item}>Как работаем</button>
+        </div>
+      </header>
+      <section className={styles.home__main}>
+        <h1>Монетизируйте клиентскую базу, не снижая NPS</h1>
+        <h2>
+          Найдите идеальный баланс выручки
+          <br />
+          и удовлетворённости пользователей с платформой
+          <br /> рекламной монетизации
+        </h2>
+        <button className={styles.home__main__action}>Заказать звонок</button>
+      </section>
+      <section className={styles.home__sources}>
+        <div>
+          <h1>
+            Дополнительные источники
+            <br /> выручки для разных
+            <br /> компаний
+          </h1>
+        </div>
+        <div className={styles.home__sources__menu}>
+          <div className={styles.home__sources__menu__buttons}>
+            <button
+              onClick={() => handleSource('operators')}
+              className={
+                source === 'operators'
+                  ? classNames(
+                      styles.home__sources__menu__buttons__item,
+                      styles.home__sources__menu__buttons__itemActive,
+                    )
+                  : styles.home__sources__menu__buttons__item
+              }
+            >
+              Операторам
+            </button>
+            <button
+              onClick={() => handleSource('services')}
+              className={
+                source === 'services'
+                  ? classNames(
+                      styles.home__sources__menu__buttons__item,
+                      styles.home__sources__menu__buttons__itemActive,
+                    )
+                  : styles.home__sources__menu__buttons__item
+              }
+            >
+              ОТТ сервисам
+            </button>
+          </div>
+          <div>
+            {source === 'operators' ? (
+              <p>
+                Найдите идеальный баланс выручки и удовлетворённости пользователей с платформой
+                рекламной монетизации
+              </p>
+            ) : source === 'services' ? (
+              <p>Дополнительные возможности обогащения данных об аудитории и монетизации</p>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      </section>
+      <section className={styles.home__promotion}>
+        <h1 className={styles.home__promotion__title}>
+          Баланс между выручкой и удовлетворённостью пользователей
+        </h1>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
